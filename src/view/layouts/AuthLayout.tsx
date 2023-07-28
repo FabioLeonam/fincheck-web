a639c4be0816d2fc1baf7d80761425a2a8c6d477
Fabio Leonam <f.leonam@hotmail.com>
2023-07-28 20:55:52 -0300

    Configures colors, creates right side of login page

--- src/view/layouts/AuthLayout.tsx
+++ src/view/layouts/AuthLayout.tsx
@@ -1,11 +1,25 @@
-import { Outlet } from "react-router-dom";
+import illustration from '../../assets/illustration.png';
+import { Logo } from '../components/Logo';
 
 export function AuthLayout() {
   return(
-    <div>
-      <h1>AuthLayout</h1>
+      <div className="flex w-full h-full">
+        <div className="w-1/2 h-full"></div>
+        <div className="w-1/2 h-full flex justify-center items-center p-8">
+          <img
+            src={illustration}
+            alt="illustration"
+            className='object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-[32px]'
+          />
 
-      <Outlet />
-    </div>
+          <div className='max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-[32px'>
+            <Logo className='text-teal-900 h-8'/>
+            <p className='text-gray-700 font-medium text-xl mt-6'>
+              Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
+            </p>
+          </div>
+        </div>
+      </div>
+
   )
 }
