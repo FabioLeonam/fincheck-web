65610f82c9f4f01561b067598b6ee699e1e1557f
Fabio Leonam <f.leonam@hotmail.com>
2023-07-30 21:19:20 -0300

    Fixes className on button component

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,12 +1,12 @@
 import { ComponentProps } from 'react';
+import { cn } from '../../app/utils/cn';
 
 interface ButtonProps extends ComponentProps<'button'>{}
-export function Button(props: ButtonProps) {
+export function Button({className, ...props}: ButtonProps) {
   return (
     <button
     {...props }
-    className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-medium
-     disabled:text-gray-400 disabled:cursor-not-allowed transition-all"
+    className={cn("bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-mediumdisabled:text-gray-400 disabled:cursor-not-allowed transition-all", className)}
     />
   )
 }
