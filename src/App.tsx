fda4807f763ac0ebac22c769fd6d3769530fa377
Fabio Leonam <f.leonam@hotmail.com>
2023-07-31 21:03:45 -0300

    Uses providers from react-query and react-hot-toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Router } from "./Router";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
 
 export function App() {
-  return <Router />
+  return(
+    <QueryClientProvider client={queryClient}>
+      <Router />
+
+      <Toaster />
+    </QueryClientProvider>
+
+  )
 }
