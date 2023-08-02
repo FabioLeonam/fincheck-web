0fde80b326e83cd868bff6fec52fd6bd58e8b44d
Fabio Leonam <f.leonam@hotmail.com>
2023-08-02 20:51:09 -0300

    Creates auth context and its hook

---
+++ src/app/contexts/AuthContext.tsx
@@ -0,0 +1,16 @@
+import React, { createContext } from "react";
+
+interface AuthContextValue {
+  signedIn: boolean;
+}
+
+export const AuthContext = createContext<AuthContextValue>({} as AuthContextValue)
+
+
+export function AuthProvider({children}: { children: React.ReactNode}) {
+  return(
+    <AuthContext.Provider value={{ signedIn: false }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
