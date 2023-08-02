6f81ff430bf195c3d7a15c3c656b5f927b008d41
Fabio Leonam <f.leonam@hotmail.com>
2023-08-02 20:23:15 -0300

    Adds spinner component

--- src/app/services/authService/signin.ts
+++ src/app/services/authService/signin.ts
@@ -1,4 +1,3 @@
-import { sleep } from "../../utils/sleep";
 import { httpClient } from "../httpClient";
 
 export interface SigninParams {
@@ -10,7 +9,6 @@ interface SignInResponse {
   accessToken: string
 }
 export async function signin(params: SigninParams) {
-  await sleep(1500);
   const { data } =  await httpClient.post<SignInResponse>('/auth/signin', params);
 
   return data;
--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,15 +1,19 @@
 import { ComponentProps } from 'react';
 import { cn } from '../../app/utils/cn';
+import { Spinner } from './Spinner';
 
 interface ButtonProps extends ComponentProps<'button'>{
   isLoading?: boolean;
 }
-export function Button({className, isLoading, disabled, ...props}: ButtonProps) {
+export function Button({className, isLoading, disabled, children,  ...props}: ButtonProps) {
   return (
     <button
     {...props }
     disabled={disabled || isLoading}
-    className={cn("bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-mediumdisabled:text-gray-400 disabled:cursor-not-allowed transition-all", className)}
-    />
+    className={cn("bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-mediumdisabled:text-gray-400 disabled:cursor-not-allowed transition-all flex items-center justify-center", className)}
+    >
+      {!isLoading && children}
+      {isLoading && <Spinner className='w-6 h-6'/>}
+    </button>
   )
 }
