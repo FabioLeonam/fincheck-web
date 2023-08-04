81d64b7dc725b1b40b8d3b2570186bbcbc434384
Fabio Leonam <f.leonam@hotmail.com>
2023-08-03 21:10:40 -0300

    Uses accounts and transactions components on dashboard

--- src/view/pages/Dashboard/Dashboard.tsx
+++ src/view/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,25 @@
+import { Logo } from "../../components/Logo";
+import { UserMenu } from "../../components/UserMenu";
+import { Accounts } from "./components/Accounts";
+import { Transactions } from "./components/Transactions";
+
 export function Dashboard() {
-  return <h1>Dashboard Page</h1>
+
+  return (
+    <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
+      <header className="h-12 flex items-center justify-between">
+        <Logo  className="h-6 text-teal-900"/>
+        <UserMenu />
+      </header>
+
+      <main className="flex flex-1 flex-col md:flex-row gap-4">
+        <div className="w-full md:w-1/2 ">
+          <Accounts />
+        </div>
+        <div className="w-full md:w-1/2 ">
+          <Transactions />
+        </div>
+      </main>
+    </div>
+  )
 }
