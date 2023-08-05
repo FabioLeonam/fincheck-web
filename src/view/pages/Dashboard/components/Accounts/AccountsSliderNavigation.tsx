96472018c88f7e7bee8c70759ff239debea8714b
Fabio Leonam <f.leonam@hotmail.com>
2023-08-05 19:19:30 -0300

    Adds props that determine the beginning and end of the slider

---
+++ src/view/pages/Dashboard/components/Accounts/AccountsSliderNavigation.tsx
@@ -0,0 +1,28 @@
+import { ChevronLeftIcon, ChevronRightIcon } from "@radix-ui/react-icons";
+import { useSwiper } from "swiper/react";
+
+interface AccountsSliderNavigationProps {
+  isBeginning:boolean;
+  isEnd: boolean
+}
+export function AccountsSliderNavigation({ isBeginning, isEnd }: AccountsSliderNavigationProps){
+  const swiper = useSwiper();
+
+  return (
+    <div>
+      <button
+        onClick={() => swiper.slidePrev()}
+        disabled={isBeginning}
+        className="py-3 pl-2.5 pr-3.5 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40">
+        <ChevronLeftIcon className="text-white w-6 h-6"/>
+      </button>
+
+      <button
+        onClick={() => swiper.slideNext()}
+        disabled={isEnd}
+        className="py-3 pl-2.5 pr-3.5 rounded-full enabled:hover:bg-black/10 transition-colors disabled:opacity-40">
+        <ChevronRightIcon className="text-white w-6 h-6"/>
+      </button>
+    </div>
+  )
+}
