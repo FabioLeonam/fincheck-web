85e157d902e7a2d44306e44723737192010dde67
Fabio Leonam <f.leonam@hotmail.com>
2023-08-06 15:52:31 -0300

    Uses DashboardProvider on dashboard page

--- src/view/pages/Dashboard/Dashboard.tsx
+++ src/view/pages/Dashboard/Dashboard.tsx
@@ -1,25 +1,29 @@
 import { Logo } from "../../components/Logo";
 import { UserMenu } from "../../components/UserMenu";
+import { DashboardProvider } from "./DashboardContext/DashboardContext";
 import { Accounts } from "./components/Accounts/Accounts";
 import { Transactions } from "./components/Transactions/Transactions";
 
 export function Dashboard() {
 
   return (
-    <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
-      <header className="h-12 flex items-center justify-between">
-        <Logo  className="h-6 text-teal-900"/>
-        <UserMenu />
-      </header>
+    <DashboardProvider>
+      <div className="h-full w-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4">
+        <header className="h-12 flex items-center justify-between">
+          <Logo  className="h-6 text-teal-900"/>
+          <UserMenu />
+        </header>
 
-      <main className="flex flex-1 flex-col md:flex-row gap-4 max-h-full">
-        <div className="w-full md:w-1/2 ">
-          <Accounts />
-        </div>
-        <div className="w-full md:w-1/2 ">
-          <Transactions />
-        </div>
-      </main>
-    </div>
+        <main className="flex flex-1 flex-col md:flex-row gap-4 max-h-full">
+          <div className="w-full md:w-1/2 ">
+            <Accounts />
+          </div>
+          <div className="w-full md:w-1/2 ">
+            <Transactions />
+          </div>
+        </main>
+      </div>
+    </DashboardProvider>
+
   )
 }
