d0e3f7c8d143e5062dfa9e924318b7b720fa9514
Fabio Leonam <f.leonam@hotmail.com>
2023-08-09 19:39:39 -0300

    Adds animation to dropdownMenu

--- src/view/components/DropdownMenu.tsx
+++ src/view/components/DropdownMenu.tsx
@@ -25,10 +25,11 @@ interface DropdownMenuContentProps {
 function DropdownMenuContent({ children, className }: DropdownMenuContentProps) {
   return(
     <RadixDropdownMenu.Portal>
-      <RadixDropdownMenu.Content className={cn(
-        "rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)]",
+      <RadixDropdownMenu.Content
+        className={cn(
+        "rounded-2xl p-2 bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] data-[side=bottom]:animate-slide-up-and-fade",
         className
-      )}>
+        )}>
         { children }
       </RadixDropdownMenu.Content>
     </RadixDropdownMenu.Portal>
