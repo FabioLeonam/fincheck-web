250f1da42e5e92d8b2a80b89e40e504ccdae15d0
Fabio Leonam <f.leonam@hotmail.com>
2023-09-05 19:40:04 -0300

    Uses react-number-format

---
+++ src/view/components/InputCurrency.tsx
@@ -0,0 +1,11 @@
+import { NumericFormat } from 'react-number-format'
+export function InputCurrency(){
+  return (
+    <NumericFormat
+      className='w-full text-gray-800 text-[32px] font-bold tracking-[-1px] outline-none'
+      thousandSeparator='.'
+      decimalSeparator=','
+      defaultValue="0"
+    />
+  )
+}
