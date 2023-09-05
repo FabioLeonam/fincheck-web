5bc8a6c99791907b5e0df043368787ba61d88a63
Fabio Leonam <f.leonam@hotmail.com>
2023-09-05 20:57:47 -0300

    Creates select component and using it

--- src/view/pages/Dashboard/components/modals/NewAccountModal/NewAccountModal.tsx
+++ src/view/pages/Dashboard/components/modals/NewAccountModal/NewAccountModal.tsx
@@ -1,4 +1,7 @@
+import { Input } from "../../../../../components/Input";
+import { InputCurrency } from "../../../../../components/InputCurrency";
 import { Modal } from "../../../../../components/Modal";
+import { Select } from "../../../../../components/Select";
 import { useNewAccountModalController } from "./useNewAccountModalCotroller";
 
 export function NewAccountModal(){
@@ -10,7 +13,40 @@ export function NewAccountModal(){
       open={isNewAccountModalOpen}
       onClose={closeNewAccountModal}
     >
-      NewAccountModal
+      <form>
+        <div>
+          <span className="text-gray-600 text-xs tracking-[-0.5px]">Saldo</span>
+          <div className="flex items-center gap-2">
+            <span className="text-gray-600 text-lg tracking-[-0.5px]">R$</span>
+            <InputCurrency />
+          </div>
+        </div>
+
+        <div className="mt-10 flex flex-col gap-4">
+          <Input
+            type="text"
+            name="name"
+            placeholder="Nome da conta"
+          />
+          <Select
+            placeholder="Tipo"
+            options={[
+            {
+              value: 'CHECKING',
+              label: 'Conta corrente'
+            },
+            {
+              value: 'INVESTMENT',
+              label: 'Investimentos'
+            },
+            {
+              value: 'CASH',
+              label: 'Dinheiro físico'
+            }
+          ]}
+          />
+        </div>
+      </form>
     </Modal>
   )
 }
