e96730edfa0e0fda8bb15ba6b90cd572ee7cbbc4
Fabio Leonam <f.leonam@hotmail.com>
2023-09-21 20:15:21 -0300

    Creates new variables in dashboardProvider

--- src/view/pages/Dashboard/DashboardContext/DashboardContext.tsx
+++ src/view/pages/Dashboard/DashboardContext/DashboardContext.tsx
@@ -6,6 +6,11 @@ interface DashboardContextValue {
   toggleValuesVisibility(): void;
   openNewAccountModal(): void;
   closeNewAccountModal(): void;
+  isNewTransactionModalOpen: boolean;
+  openNewTransactionModal(type: 'INCOME' | 'EXPENSE'): void;
+  closeNewTransactionModal(): void;
+  newTransactionType: "INCOME" | "EXPENSE" | null;
+  setNewTransactionType(type: 'INCOME' | 'EXPENSE' | null): void;
 }
 
 export const DashboardContext = createContext({} as DashboardContextValue)
@@ -14,7 +19,9 @@ export const DashboardContext = createContext({} as DashboardContextValue)
 export function DashboardProvider({ children }: { children: React.ReactNode}){
 
   const [areValuesVisible, setAreValuesVisible] = useState(true);
-  const [isNewAccountModalOpen, setIsNewAccountModalOpen] = useState(true);
+  const [isNewAccountModalOpen, setIsNewAccountModalOpen] = useState(false);
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+  const [newTransactionType, setNewTransactionType] = useState<'INCOME' | 'EXPENSE' | null>(null)
 
   const toggleValuesVisibility = useCallback(() => {
     setAreValuesVisible((prevState) => !prevState)
@@ -28,6 +35,16 @@ export function DashboardProvider({ children }: { children: React.ReactNode}){
     setIsNewAccountModalOpen(false)
   }, []);
 
+  const openNewTransactionModal = useCallback((type: 'INCOME' | 'EXPENSE') => {
+    setNewTransactionType(type)
+    setIsNewTransactionModalOpen(true)
+  }, []);
+
+  const closeNewTransactionModal = useCallback(() => {
+    setNewTransactionType(null)
+    setIsNewTransactionModalOpen(false)
+  }, []);
+
 
   return(
     <DashboardContext.Provider value={{
@@ -35,7 +52,12 @@ export function DashboardProvider({ children }: { children: React.ReactNode}){
       toggleValuesVisibility,
       isNewAccountModalOpen,
       openNewAccountModal,
-      closeNewAccountModal
+      closeNewAccountModal,
+      isNewTransactionModalOpen,
+      openNewTransactionModal,
+      closeNewTransactionModal,
+      newTransactionType,
+      setNewTransactionType
     }}>
       {children}
     </DashboardContext.Provider>
