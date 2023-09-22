a6561ccf76d603d29f927a3a485d32884782cf5c
Fabio Leonam <f.leonam@hotmail.com>
2023-09-21 21:15:36 -0300

    Creates a generic Popover

---
+++ src/view/components/Popover.tsx
@@ -0,0 +1,44 @@
+import * as RadixPopover from '@radix-ui/react-popover'
+import { cn } from '../../app/utils/cn';
+
+function PopoverRoot({ children }: { children: React.ReactNode}) {
+  return(
+    <RadixPopover.Root>
+      { children }
+    </RadixPopover.Root>
+  );
+}
+
+function PopoverTrigger({ children }: { children: React.ReactNode}) {
+  return(
+    <RadixPopover.Trigger asChild>
+      { children }
+    </RadixPopover.Trigger>
+  );
+}
+
+interface PopoverContentProps {
+  children: React.ReactNode;
+  className?: string;
+}
+function PopoverContent({ children, className }: PopoverContentProps) {
+  return(
+    <RadixPopover.Portal>
+      <RadixPopover.Content
+        className={cn(
+        "rounded-2xl bg-white space-y-2 shadow-[0px_11px_20px_0px_rgba(0,0,0,0.10)] z-[99] p-4",
+        "data-[side=bottom]:animate-slide-up-and-fade data-[side=top]:animate-slide-down-and-fade",
+        className
+        )}>
+        { children }
+      </RadixPopover.Content>
+    </RadixPopover.Portal>
+  );
+}
+
+
+export const Popover = {
+  Root: PopoverRoot,
+  Trigger: PopoverTrigger,
+  Content: PopoverContent
+}
