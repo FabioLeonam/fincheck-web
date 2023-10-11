d9ab0f3e87e6e1bef77ced73127e5aa2ba9514a6
Fabio Leonam <f.leonam@hotmail.com>
2023-10-11 20:16:41 -0300

    Adds types to Button component

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -4,13 +4,17 @@ import { Spinner } from './Spinner';
 
 interface ButtonProps extends ComponentProps<'button'>{
   isLoading?: boolean;
+  variant?: 'danger' | 'ghost';
 }
-export function Button({className, isLoading, disabled, children,  ...props}: ButtonProps) {
+export function Button({className, isLoading, disabled, children, variant, ...props}: ButtonProps) {
   return (
     <button
     {...props }
     disabled={disabled || isLoading}
-    className={cn("bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-mediumdisabled:text-gray-400 disabled:cursor-not-allowed transition-all flex items-center justify-center", className)}
+    className={cn("bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-mediumdisabled:text-gray-400 disabled:cursor-not-allowed transition-all flex items-center justify-center",
+    variant === 'danger' && 'bg-red-900 hover:bg-red-800',
+    variant === 'ghost' && 'bg-transparent border border-gray-800 text-gray-800 hover:bg-gray-800/5',
+    className)}
     >
       {!isLoading && children}
       {isLoading && <Spinner className='w-6 h-6'/>}
