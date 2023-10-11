09c6fb2bb46a1c1f58983d298fae06886245921f
Fabio Leonam <f.leonam@hotmail.com>
2023-10-11 20:17:03 -0300

    Creates remove bankAccount service

--- src/app/services/bankAccountsService/index.ts
+++ src/app/services/bankAccountsService/index.ts
@@ -1,8 +1,10 @@
 import { create } from "./create"
 import { getAll } from "./getAll"
 import { update } from "./update"
+import { remove } from "./remove"
 export const bankAccountService = {
   create,
   getAll,
-  update
+  update,
+  remove
 }
