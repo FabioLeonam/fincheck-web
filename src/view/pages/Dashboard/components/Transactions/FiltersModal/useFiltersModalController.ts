faea2bfe468e94f2ac194109bc0704fbb670c8d4
Fabio Leonam <f.leonam@hotmail.com>
2023-10-17 20:23:22 -0300

    Renames an UI controller

--- src/view/pages/Dashboard/components/Transactions/FiltersModal/useFiltersModalController.ts
+++ src/view/pages/Dashboard/components/Transactions/FiltersModal/useFiltersModalController.ts
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { useBankAccounts } from "../../../../../../app/hooks/useBankAccounts";
 
-export function useFiltersModal(){
-  const [selectedBankAccountId, setSelectedBankAccountId] = useState<null | string>(null);
+export function useFiltersModalController(){
+  const [selectedBankAccountId, setSelectedBankAccountId] = useState<undefined | string>(undefined);
   const [selectedYear, setSelectedYear] = useState(new Date().getFullYear())
+
+  const { accounts } = useBankAccounts()
 
   function handleSelectBankAccount(bankAccountId: string){
     setSelectedBankAccountId((prevState) =>
       prevState === bankAccountId
-        ? null
+        ? undefined
         : bankAccountId
     )
   }
@@ -20,6 +23,7 @@ export function useFiltersModal(){
     handleSelectBankAccount,
     selectedBankAccountId,
     selectedYear,
-    handleChangeYear
+    handleChangeYear,
+    accounts
   }
 }
